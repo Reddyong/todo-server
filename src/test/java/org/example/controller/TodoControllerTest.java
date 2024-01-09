bcda01c856bceffa5be013f2745b0cf7c1760169
Reddyong <d0209hong@naver.com>
2024-01-09 18:12:01 +0900

    feat : TodoServiceTest

---
+++ src/test/java/org/example/controller/TodoControllerTest.java
@@ -0,0 +1,19 @@
+package org.example.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TodoController.class)
+class TodoControllerTest {
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void readOne() {
+    }
+}
